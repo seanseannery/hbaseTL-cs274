6a2d7d8f254b66c68ee01004247e2373ae159263
sean_seannery <im.sean@gmail.com>
2012-03-07 01:16:18 -0800

    Added empty classes for shits and giggles

---
+++ src/HBaseTLParser.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+/**
+ * @author sam
+ *
+ */
+public class HBaseTLParser {
+
+	//does stuff... maybe processes new statements such as begin and commit.
+	
+}
---
+++ src/HBaseTLScheduler.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+/**
+ * @author sam
+ *
+ */
+public class HBaseTLScheduler {
+
+	//does stuff... maybe schedules and tests history correctness stuff.
+	//this may have to be multi-threaded...
+	
+}
